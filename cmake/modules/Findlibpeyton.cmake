5aa6987f0925cfbe83aa381c3e96886f1c445d64
Mario Juric <mjuric@ias.edu>
2010-05-25 22:57:16 -0400

    Findlibpeyton: libpeyton_ROOT can be a build dir
    Reinterpret libpeyton_ROOT as referring to a build directory within the root
    source directory, if the package isn't found through standard means.

--- cmake/modules/Findlibpeyton.cmake
+++ cmake/modules/Findlibpeyton.cmake
@@ -1,4 +1,9 @@
-# - Try to find libpeyton
+# Try to find libpeyton. Look for it in
+#  1) Standard include/library paths
+#  2) libpeyton_ROOT/lib and libpeyton_ROOT/include
+#  3) libpeyton_ROOT (for lib) and libpeyton_ROOT/include plus libpeyton_ROOT/../include
+#     (this is the usual setup if libpeyton_ROOT is an in-source build directory)
+#
 # Once done this will define
 #
 #  libpeyton_FOUND - system has LIBPEYTON
@@ -8,6 +13,7 @@
 #  If libpeyton_ROOT is set, look for it there
 #
 
+# Copyright (c) 2010, Mario Juric <mjuric@ias.edu>
 # Copyright (c) 2006, Jasem Mutlaq <mutlaqja@ikarustech.com>
 # Based on FindLibfacile by Carsten Niehaus, <cniehaus@gmx.de>
 #
@@ -22,13 +28,40 @@ if (libpeyton_INCLUDE_DIR AND libpeyton_LIBRARIES)
 
 else (libpeyton_INCLUDE_DIR AND libpeyton_LIBRARIES)
 
-  find_path(libpeyton_INCLUDE_DIR astro/peyton_version.h
-    PATHS ${libpeyton_ROOT}/include
-  )
+    find_path(libpeyton_INCLUDE_DIR astro/version.h
+      PATHS ${libpeyton_ROOT}/include
+    )
 
-  find_library(libpeyton_LIBRARIES NAMES peyton
-    PATHS ${libpeyton_ROOT}/lib
-  )
+    find_library(libpeyton_LIBRARIES NAMES peyton
+      PATHS ${libpeyton_ROOT}/lib
+    )
+
+  if(NOT (libpeyton_INCLUDE_DIR AND libpeyton_LIBRARIES) AND libpeyton_ROOT)
+    # Reinterpret libpeyton_ROOT as referring to a build directory
+    # within the root source directory, and try to find include+lib files
+
+    # Find generated include files
+    find_path(libpeyton_INCLUDE_DIR_static astro/peyton_version.h
+      PATHS ${libpeyton_ROOT}/include
+    )
+
+    # Find static include files
+    find_path(libpeyton_INCLUDE_DIR_gen astro/version.h
+      PATHS ${libpeyton_ROOT}/../include
+    )
+
+    # Set libpeyton_INCLUDE_DIR only if both static and generated include
+    # files were found.
+    if (libpeyton_INCLUDE_DIR_gen AND libpeyton_INCLUDE_DIR_static)
+      set (libpeyton_INCLUDE_DIR ${libpeyton_INCLUDE_DIR_static} ${libpeyton_INCLUDE_DIR_gen})
+    endif (libpeyton_INCLUDE_DIR_gen AND libpeyton_INCLUDE_DIR_static)
+
+    # Find the library
+    find_library(libpeyton_LIBRARIES NAMES peyton
+      PATHS ${libpeyton_ROOT}
+    )
+  endif(NOT (libpeyton_INCLUDE_DIR AND libpeyton_LIBRARIES) AND libpeyton_ROOT)
+
 
   if(libpeyton_INCLUDE_DIR AND libpeyton_LIBRARIES)
     set(libpeyton_FOUND TRUE)
@@ -38,11 +71,12 @@ else (libpeyton_INCLUDE_DIR AND libpeyton_LIBRARIES)
 
   if (libpeyton_FOUND)
     if (NOT libpeyton_FIND_QUIETLY)
+      message(STATUS "Found libpeyton ${libpeyton_VERSION_STRING}: ${libpeyton_INCLUDE_DIR}")
       message(STATUS "Found libpeyton ${libpeyton_VERSION_STRING}: ${libpeyton_LIBRARIES}")
     endif (NOT libpeyton_FIND_QUIETLY)
   else (libpeyton_FOUND)
     if (libpeyton_FIND_REQUIRED)
-      message(SEND_ERROR "libpeyton not found.")
+      message(FATAL_ERROR "libpeyton not found.")
     else (libpeyton_FIND_REQUIRED)
       message(STATUS "libpeyton not found.")
     endif (libpeyton_FIND_REQUIRED)
